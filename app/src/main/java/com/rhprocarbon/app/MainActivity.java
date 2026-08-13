aec8c05132dddc5b22946bcaf67381928830a84b
rhprocarbonbandaaceh-ops <rhprocarbonbandaaceh@gmail.com>
2026-08-13 12:48:17 +0700

    Update MainActivity.java

--- app/src/main/java/com/rhprocarbon/app/MainActivity.java
+++ app/src/main/java/com/rhprocarbon/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.rhprocarbon.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -18,7 +20,31 @@ public class MainActivity extends Activity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                if (url.startsWith("whatsapp://")) {
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(url));
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        // WhatsApp tidak tersedia
+                    }
+
+                    return true;
+                }
+
+                if (url.startsWith("http://") || url.startsWith("https://")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+
+                return true;
+            }
+        });
 
         webView.loadUrl("https://rhprocarbonbandaaceh-ops.github.io/rhprocarbon_bna.com/");
 
